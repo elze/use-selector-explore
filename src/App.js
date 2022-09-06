7c4d256a651aabce5c95f1afa59916662715b0f4
Elze Hamilton <elze.hamilton@gmail.com>
2022-09-05 21:14:23 -0500

    Created a React app that uses a useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+
 import './App.css';
+
+const Apple = () => {	
+	const dispatch = useDispatch();
+	const appleState = useSelector(
+		(state) => {
+		// console.log(`Apple useSelector: state = ${JSON.stringify(state)}`)
+		return state.apple;
+		}, 
+		shallowEqual 
+	);	
+	const num = parseInt(appleState.split(" ")[1]);
+	console.log(`Apple num = ${num}`);	
+    return (
+	<div>
+      <button type="button" className="my-button"
+				onClick={() => dispatch({type: 'incrementCount', name: "apple", num: num})}
+	  >
+		Apple
+      </button>
+	  <div className="my-component">
+		<img src="https://blog.geekitude.com/wp-content/uploads/2022/09/braeburn_variety-page-50x50-1.png" alt="Braeburn apple"/>{appleState}
+	  </div>
+	</div>
+    );
+}
+	  
+const Mushroom = () => {	
+	const dispatch = useDispatch();
+	const mushroomState = useSelector(
+		(state) => {
+		// console.log(`Mushroom useSelector: state = ${JSON.stringify(state)}`)
+		return state.mushroom;
+		}, 
+		shallowEqual 
+	  );	
+	const num = parseInt(mushroomState.split(" ")[1]);
+	console.log(`Mushroom num = ${num}`);
+	return (
+	<div>
+	  <button type="button" className="my-button"
+			onClick={() => dispatch({type: 'incrementCount', name: "mushroom", num: num})}
+	  >
+		Mushroom
+	  </button>
+	  <div className="my-component">
+		<img src="https://blog.geekitude.com/wp-content/uploads/2022/09/portobello-2006620_1280_50.png" alt="Portabella mushrooms"/>{mushroomState}
+	  </div>
+	</div>
+	);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<p>Click either "Apple" or "Mushroom" button. The counter in the corresponding white box will increase. With React Dev Tools enabled in the browser, it will show which component has re-rendered.</p>
+		<Apple/>
+		<Mushroom/>
     </div>
   );
 }
---
+++ src/reducers.js
@@ -0,0 +1,24 @@
+import { shallowEqual } from 'react-redux';
+
+export const originalState = {
+	apple: `Braeburn 1`,
+    mushroom: `Portabella 1`	
+};
+
+
+export function reducer(state=originalState, action) {  
+  switch (action.type) {
+      case 'incrementCount': 
+		const num = (action.num + 1) % 10;
+		if (action.name === "apple") {
+			const newState = {...state, apple: `Braeburn ${num}`}
+			console.log(`reducer, case toggleButton: action.name === ${action.name} state = ${JSON.stringify(state)} newState = ${JSON.stringify(newState)} shallowEqual(state.apple, newState.apple) = ${shallowEqual(state.apple, newState.apple)} shallowEqual(state.mushroom, newState.mushroom) = ${shallowEqual(state.mushroom, newState.mushroom)}`);
+			return newState;
+		}
+		const newState = {...state, mushroom: `Portabella ${num}`}
+		console.log(`reducer, case toggleButton: action.name === ${action.name} state = ${JSON.stringify(state)} newState = ${JSON.stringify(newState)} shallowEqual(state.apple, newState.apple) = ${shallowEqual(state.apple, newState.apple)} shallowEqual(state.mushroom, newState.mushroom) = ${shallowEqual(state.mushroom, newState.mushroom)}`);			
+		return newState;	  
+      default:
+	      return state;
+  }
+}
